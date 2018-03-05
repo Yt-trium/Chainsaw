240943ece410d83be20a1e8fc68e2c15033b7e39
Yt-trium <cyril.meyer@etu.unistra.fr>
2018-03-05 04:56:30 +0100

    basic attributes and methods and implementation of setSettings method

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,75 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <cmath>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    pixmap  = new QPixmap(32,32);
+    painter = new QPainter(pixmap);
+    pen     = new QPen(Qt::white);
+
+    painter->setPen(*pen);
+
+    randomEngine.seed(std::chrono::system_clock::now().time_since_epoch().count());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_generate_clicked()
+{
+    setSettings();
+}
+
+void MainWindow::on_pushButton_preview_clicked()
+{
+    setSettings();
+    genNewPixmap()
+}
+
+void MainWindow::setSettings()
+{
+    int i, j, p;
+    int size, depth;
+
+    randomIntX.clear();
+    randomIntY.clear();
+
+    size    = ui->spinBox_size->value();
+    depth   = ui->spinBox_depth->value();
+
+    std::uniform_int_distribution<int> tmpRandom;
+
+    for(i=0;i<=depth;++i)
+    {
+        tmpRandom = std::uniform_int_distribution<int>(i*(size/(depth+1)),
+                                                      (i+1)*(size/(depth+1)));
+        randomIntX.push_back(tmpRandom);
+
+        if(i==0)
+            p = 1;
+        else if(i==1)
+            p = 2;
+        else
+            p = std::pow(i,2);
+
+        for(j=0;j<p;j++)
+        {
+            tmpRandom = std::uniform_int_distribution<int>(j*(size/p),
+                                                          (j+1)*(size/p));
+            randomIntY.push_back(tmpRandom);
+        }
+    }
+}
+
+void MainWindow::genNewPixmap()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+#include <algorithm>
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +17,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_generate_clicked();
+
+    void on_pushButton_preview_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    void setSettings();
+    void genNewPixmap();
+
+    std::default_random_engine                          randomEngine;
+    std::vector<std::uniform_int_distribution<int>>     randomIntX;
+    std::vector<std::uniform_int_distribution<int>>     randomIntY;
+
+    QPixmap     *pixmap;
+    QPainter    *painter;
+    QPen        *pen;
 };
 
 #endif // MAINWINDOW_H
